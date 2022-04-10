ad25c56c2a3a255ff4f8f4b4edb42d4fb43da9b0
Lil <choreiwken@outlook.com>
2022-04-11 02:05:25 +0700

    first commit

---
+++ src/components/MenuItems.jsx
@@ -0,0 +1,37 @@
+import { useLocation } from "react-router";
+import { Menu } from "antd";
+import { NavLink } from "react-router-dom";
+
+function MenuItems() {
+  const { pathname } = useLocation();
+
+  return (
+    <Menu
+      theme="light"
+      mode="horizontal"
+      style={{
+        display: "flex",
+        fontSize: "20px",
+        fontWeight: "300",
+        width: "100%",
+        justifyContent: "center",
+      }}
+      defaultSelectedKeys={[pathname]}
+    >
+      <Menu.Item key="/1inch">
+        <NavLink to="/1inch">🏦 Dex</NavLink>
+      </Menu.Item>
+      <Menu.Item key="/erc20balance">
+        <NavLink to="/erc20balance">💰 Balances</NavLink>
+      </Menu.Item>
+      <Menu.Item key="/erc20transfers">
+        <NavLink to="/erc20transfers">💸 Transfers</NavLink>
+      </Menu.Item>
+      <Menu.Item key="/nftBalance">
+        <NavLink to="/nftBalance">🖼 NFTs</NavLink>
+      </Menu.Item>
+    </Menu>
+  );
+}
+
+export default MenuItems;
